8748d3a6901cd78f3d1e8586aaf43c9ed0e9980a
AhMeD HaMeD - احمد حامد <39957737+TechnoHamed@users.noreply.github.com>
2023-07-08 02:42:15 +0300

    5.cpp

--- Website-Assignments/16-23/5.cpp
+++ Website-Assignments/16-23/5.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    char a = '~';
-    char b = '&';
-    char c = '%';
-    char d = 'A';
-    
-    // Output Needed
-    // "ASCII Value of ~ Is 126"
-    cout << "ASCII Value of " << a << " Is " << int(a) << endl;
-    // "ASCII Value of & Is 38"
-    cout << "ASCII Value of " << b << " Is " << int(b) << endl;
-    // "ASCII Value of % Is 37"
-    cout << "ASCII Value of " << c << " Is " << int(c) << endl;
-    // "ASCII Value of A Is 65"
-    cout << "ASCII Value of " << d << " Is " << int(d) << endl;
+int main()
+{
 
-    return 0;
+  char a = '~';
+  char b = '&';
+  char c = '%';
+  char d = 'A';
+
+/*  Output Needed
+  "ASCII Value of ~ Is 126"
+  "ASCII Value of & Is 38"
+  "ASCII Value of % Is 37"
+  "ASCII Value of A Is 65" */
+
+
+  cout << "ASCII Value of " << a << " IS " << int(a) << endl;
+  cout << "ASCII Value of " << b << " IS " << int(b) << endl;
+  cout << "ASCII Value of " << c << " IS " << int(c) << endl;
+  cout << "ASCII Value of " << d << " IS " << int(d) << endl;
+
+  return 0;
 }
