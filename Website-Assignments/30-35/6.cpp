58d636e8e9b0744f834b6814a6ce26977b5d66ef
AhMeD HaMeD - احمد حامد <39957737+TechnoHamed@users.noreply.github.com>
2023-07-24 03:38:28 +0300

    6.cpp

--- Website-Assignments/30-35/6.cpp
+++ Website-Assignments/30-35/6.cpp
@@ -4,13 +4,17 @@ using namespace std;
 int main() {
     int age = 40;
     int points = 100;
-    
+
+    // Solve #1
     if (age > 18 && points > 50 && sizeof(age) == 4)
-    {
-        cout << "Age Data Is 4 Bytes\n";
+    {   cout << "Age Data Is 4 Bytes\n";
         cout << "Age Is Ok\n";
-        cout << "Points Is Ok\n";
-    }
+        cout << "Points Is Ok\n";}
+
+    // Solve #2
+    if (points > 50 && sizeof(age) == 4 && age > 18)cout << "Age Data Is 4 Bytes\nAge Is Ok\nPoints Is Ok\n";
+    
+    // Solve #1
     
     // Output Needed
     // "Age Data Is 4 Bytes"
